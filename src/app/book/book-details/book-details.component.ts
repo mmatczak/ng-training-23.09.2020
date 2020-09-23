224c9266e3fcbb40ff593fb1c739d5b31bcd523c
Marek Matczak <mmatczak@autograf.pl>
2020-09-23 14:06:15 +0200

    simple component

---
+++ src/app/book/book-details/book-details.component.ts
@@ -0,0 +1,18 @@
+import {Component} from '@angular/core';
+import {Book} from '../book';
+
+@Component({
+  selector: 'app-book-details',
+  templateUrl: './book-details.component.html',
+  styleUrls: ['./book-details.component.scss']
+})
+export class BookDetailsComponent {
+  book: Book;
+
+  constructor() {
+    this.book = {
+      author: 'John Example',
+      title: 'Some title'
+    };
+  }
+}
