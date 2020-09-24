237dc09414c709a2efeb9fdafaa3e9150b8b28a9
Marek Matczak <mmatczak@autograf.pl>
2020-09-24 11:11:04 +0200

    BookOverview tested

--- src/app/book/book-overview/book-overview.component.spec.ts
+++ src/app/book/book-overview/book-overview.component.spec.ts
@@ -1,14 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BookOverviewComponent } from './book-overview.component';
+import {BookDetailsComponent} from '../book-details/book-details.component';
 
 describe('BookOverviewComponent', () => {
   let component: BookOverviewComponent;
   let fixture: ComponentFixture<BookOverviewComponent>;
+  let element: HTMLElement;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ BookOverviewComponent ]
+    return TestBed.configureTestingModule({
+      declarations: [ BookOverviewComponent, BookDetailsComponent ]
     })
     .compileComponents();
   });
@@ -16,10 +18,27 @@ describe('BookOverviewComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(BookOverviewComponent);
     component = fixture.componentInstance;
+    element = fixture.nativeElement;
     fixture.detectChanges();
   });
 
-  xit('should create', () => {
-    expect(component).toBeTruthy();
+  it ('renders book rows', () => {
+    const rowElements = element.querySelectorAll<HTMLTableRowElement>('table > tbody > tr');
+    expect(rowElements.length).toBe(3);
+  });
+
+  it ('renders book details on row click', () => {
+    // given
+    const rowElements = element.querySelectorAll<HTMLTableRowElement>('table > tbody > tr');
+    const firstRow = rowElements[0];
+    // when
+    firstRow.click();
+    fixture.detectChanges();
+    // then
+    const bookDetailsElement = element.querySelector('app-book-details');
+    expect(bookDetailsElement).toBeDefined();
+    const authorInput = bookDetailsElement.querySelector<HTMLInputElement>('#author');
+    expect(bookDetailsElement).toBeDefined();
+    expect(authorInput.value).toBe(component.selectedBook.author);
   });
 });
