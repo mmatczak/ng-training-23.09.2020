7dc38af4b8524abaef3591b989cab89595d7310e
Marek Matczak <mmatczak@autograf.pl>
2020-09-24 14:53:08 +0200

    Simple typeahead

--- src/app/book/book-overview/book-overview.component.ts
+++ src/app/book/book-overview/book-overview.component.ts
@@ -1,19 +1,43 @@
-import {Component} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnDestroy, ViewChild} from '@angular/core';
 import {Book} from '../book';
 import {BookService} from '../book.service';
-import {Observable} from 'rxjs';
+import {fromEvent, Observable, Subject} from 'rxjs';
+import {debounceTime, distinct, distinctUntilChanged, map, takeUntil} from 'rxjs/operators';
 
 @Component({
   selector: 'app-book-overview',
   templateUrl: './book-overview.component.html',
   styleUrls: ['./book-overview.component.scss']
 })
-export class BookOverviewComponent {
+export class BookOverviewComponent implements AfterViewInit, OnDestroy {
   books$: Observable<Book[]>;
   selectedBook: Book | null = null;
 
+  @ViewChild('search')
+  searchInput: ElementRef<HTMLInputElement>;
+
+  private readonly unsubscribe = new Subject();
+
   constructor(private readonly books: BookService) {
     this.books$ = books.getAll();
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe.next();
+    this.unsubscribe.complete();
+  }
+
+  ngAfterViewInit(): void {
+    fromEvent(this.searchInput.nativeElement, 'keyup')
+      .pipe(
+        debounceTime(500),
+        map(event => (event.target as HTMLInputElement).value),
+        distinctUntilChanged(),
+        takeUntil(this.unsubscribe)
+      )
+      .subscribe(searchText => {
+        console.log(searchText);
+      });
   }
 
   selectBook(book: Book): void {
@@ -26,6 +50,9 @@ export class BookOverviewComponent {
 
   updateBook(bookToUpdate: Book): void {
     this.books.update(bookToUpdate)
+      .pipe(
+        takeUntil(this.unsubscribe)
+      )
       .subscribe(book => this.selectBook(book));
   }
 }
